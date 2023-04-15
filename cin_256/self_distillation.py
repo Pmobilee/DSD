a5e333bb4ea948852c3e64852445ebbafd4e4a01
Pmobilee <muldamion@gmail.com>
2023-04-15 15:57:23 +0200

    current

--- cin_256/self_distillation.py
+++ cin_256/self_distillation.py
@@ -49,30 +49,24 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
     instance = 0
     generation = 0
     all_losses = []
-    halve_step = ddim_steps_student
-    step_reduction_iterations = []
+
     if step_scheduler == "iterative":
         halvings = math.floor(math.log(32)/math.log(2)) + 1
         updates_per_halving = int(gradient_updates / halvings)
-            
     intermediate_generation_compare = int(gradient_updates * 0.8 / 10)
-    # if os.path.exists(f"{cwd}/saved_images/FID/{run_name}"):
-    #     print("FID folder exists")
-    #     shutil.rmtree(f"{cwd}/saved_images/FID/{run_name}")
-    
-
+    if step_scheduler == "FID":
+        if os.path.exists(f"{cwd}/saved_images/FID/{run_name}"):
+            print("FID folder exists")
+            shutil.rmtree(f"{cwd}/saved_images/FID/{run_name}")
 
     with torch.no_grad():
-        with student.ema_scope():
-                
+        with student.ema_scope():              
                 sampler_student.make_schedule(ddim_num_steps=ddim_steps_student, ddim_eta=ddim_eta, verbose=False)
                 sampler_original.make_schedule(ddim_num_steps=ddim_steps_student, ddim_eta=ddim_eta, verbose=False)
-                sc = student.get_learned_conditioning(
-                            {student.cond_stage_key: torch.tensor(1*[1000]).to(student.device)}
-                            )
+                sc = student.get_learned_conditioning({student.cond_stage_key: torch.tensor(1*[1000]).to(student.device)})
                 
-                # if step_scheduler =="FID":
-                #     current_fid = util.get_fid(student, sampler_student, num_imgs=100, name=run_name, instance = 0, steps=[ddim_steps_student])
+                if step_scheduler =="FID":
+                    current_fid = util.get_fid(student, sampler_student, num_imgs=100, name=run_name, instance = 0, steps=[ddim_steps_student])
                 
                 updates = ddim_steps_student
 
@@ -99,7 +93,7 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                             # sampler_student.make_schedule(ddim_num_steps=ddim_steps_student, ddim_eta=ddim_eta, verbose=False)
                                             optimizer.zero_grad()
 
-                                            samples_ddim, pred_x0_student, st, at= sampler_student.sample_student(S=1,
+                                            samples_ddim, pred_x0_student, _, at= sampler_student.sample_student(S=1,
                                                                             conditioning=c_student,
                                                                             batch_size=1,
                                                                             shape=[3, 64, 64],
@@ -113,18 +107,14 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                                                             intermediate_step = steps*2,
                                                                             steps_per_sampling = 1,
                                                                             total_steps = ddim_steps_student)
-                                            
-                                    
-                                        
 
                                         with torch.no_grad():
-                                            samples_ddim, teacher_intermediate, x_T, pred_x0_teacher, a_t_teacher = sampler_student.sample(S=1,
+                                            samples_ddim, _, _, pred_x0_teacher, _ = sampler_student.sample(S=1,
                                                                         conditioning=c_student,
                                                                         batch_size=1,
                                                                         shape=[3, 64, 64],
                                                                         verbose=False,
                                                                         x_T=samples_ddim,
-                                                                        
                                                                         unconditional_guidance_scale=scale,
                                                                         unconditional_conditioning=sc, 
                                                                         eta=ddim_eta,
@@ -138,20 +128,11 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                             noise = 1 - at
                                             log_snr = torch.log(signal / noise)
                                             weight = max(log_snr, 1)
-                                            
-
                                             loss = weight * criterion(pred_x0_student, pred_x0_teacher.detach())
-                                        
-            
-                                    
                                             loss.backward()
-                                            
-                                            
                                             # torch.nn.utils.clip_grad_norm_(sampler_student.model.parameters(), 1)
-                                            
                                             optimizer.step()
                                             # scheduler.step()
-                                            
                                             losses.append(loss.item())
                                             
                                         if session != None:
@@ -166,7 +147,7 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                         
 
                                         if session != None and instance % 10000 == 0 and generation > 0:
-                                            fids = util.get_fid(student, sampler_student, num_imgs=100, name=run_name, instance = instance+1, steps=[64, 32, 16, 8, 4, 2, 1])
+                                            fids = util.get_fid(student, sampler_student, num_imgs=100, name=run_name, instance = instance+1, steps=[32, 16, 8, 4, 2, 1])
                                             session.log({"fid_32":fids[0]})
                                             session.log({"fid_16":fids[1]})
                                             session.log({"fid_8":fids[2]})
@@ -182,42 +163,26 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                                 images, _ = util.compare_teacher_student_with_schedule(original, sampler_original, student, sampler_student, steps=[64, 32, 16, 8,  4, 2, 1], prompt=992)
                                                 images = wandb.Image(_, caption="left: Teacher, right: Student")
                                                 wandb.log({"schedule": images})
-
                                                 sampler_student.make_schedule(ddim_num_steps=ddim_steps_student, ddim_eta=ddim_eta, verbose=False)
                                                 sampler_original.make_schedule(ddim_num_steps=ddim_steps_student, ddim_eta=ddim_eta, verbose=False)
 
-                                                
-                                    
-                                            
+                            if generation > 0 and generation % 20 == 0 and ddim_steps_student != 1 and step_scheduler=="FID":
+                                fid = util.get_fid(student, sampler_student, num_imgs=100, name=run_name, 
+                                            instance = instance, steps=[ddim_steps_student])
+                                if fid[0] <= current_fid[0] * 0.9 and decrease_steps==True:
+                                    print(fid[0], current_fid[0])
+                                    if ddim_steps_student in [16, 8, 4, 2, 1]:
+                                        name = "intermediate"
+                                        saving_loading.save_model(sampler_student, optimizer, scheduler, name, steps * 2, run_name)
+                                    if ddim_steps_student != 2:
+                                        ddim_steps_student -= 2
+                                        updates -= 1
+                                    else:
+                                        ddim_steps_student = 1
+                                        updates = 1    
+                                    current_fid = fid
+                                    print("steps decresed:", ddim_steps_student)    
 
-                                        
-                                        
-
-                                    
-                            # if generation > 0 and generation % 20 == 0 and ddim_steps_student != 1 and step_scheduler=="FID":
-                            #     fid = util.get_fid(student, sampler_student, num_imgs=100, name=run_name, 
-                            #                 instance = instance, steps=[ddim_steps_student])
-                            #     if fid[0] <= current_fid[0] * 0.9 and decrease_steps==True:
-                            #         print(fid[0], current_fid[0])
-                            #         if ddim_steps_student in [16, 8, 4, 2, 1]:
-                            #             name = "intermediate"
-                            #             saving_loading.save_model(sampler_student, optimizer, scheduler, name, steps * 2, run_name)
-                            #         if ddim_steps_student != 2:
-                            #             ddim_steps_student -= 2
-                            #             updates -= 1
-                                        
-
-                            #         else:
-                            #             ddim_steps_student = 1
-                            #             updates = 1    
-
-                            #         current_fid = fid
-                            #         print("steps decresed:", ddim_steps_student)    
-
-                                
-                                
-
-                            
                             if session != None:
                                 with torch.no_grad():
                                     if generation > 0 and generation % intermediate_generation_compare == 0 and session !=None:
@@ -227,27 +192,13 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                         del img, grid, predictions_temp, x_T_student_decode, x_T_teacher_decode, student_target, teacher_target
                                         torch.cuda.empty_cache()
                             
-                            
-                            
-                            
                             all_losses.extend(losses)
-                            # print(scheduler.get_last_lr())
                             averaged_losses.append(sum(losses) / len(losses))
                             if session != None:
                                 session.log({"generation_loss":averaged_losses[-1]})
                             tepoch.set_postfix(epoch_loss=averaged_losses[-1])
                             torch.cuda.empty_cache()
-                            if early_stop == True and i > 1:
-                                if averaged_losses[-1] > (10*losses[-2]):
-                                    print(f"Early stop initiated: Prev loss: {round(averaged_losses[-2], 5)}, Current loss: {round(averaged_losses[-1], 5)}")
-                                    plt.plot(range(len(averaged_losses)), averaged_losses, label="MSE LOSS")
-                                    plt.xlabel("Generations")
-                                    plt.ylabel("px MSE")
-                                    plt.title("MSEloss student vs teacher")
-                                    plt.show()
-                                 
-
-                                                            
+                                                                           
     plt.plot(range(len(all_losses)), all_losses, label="MSE LOSS")
     plt.xlabel("Generations")
     plt.ylabel("px MSE")
