4ce601f28e4ed443afa56c5eb882c8029e973515
Pmobilee <muldamion@gmail.com>
2023-04-19 08:58:18 +0200

    from snellius

--- cin_256/SI_Batch.sh
+++ cin_256/SI_Batch.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=Cuda_Test
+#SBATCH --job-name=CIN_Distillation
+#SBATCH --cpus-per-task=18
 #SBATCH --ntasks=1
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --mem=20G
-#SBATCH --time=00:05:00
+#SBATCH --mem=16G
+#SBATCH --time=03:00:00
 
 # Execute Program
-
 module load 2022
- 
-source activate D-SD
+module load CUDA/11.7.0
+module load cuDNN/8.4.1.50-CUDA-11.7.0
+# module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
+source $HOME/.bashrc
+conda activate D-SD
 
-srun python distill.py -t SI 
+cd $Home/thesis/Diffusion_Thesis/cin_256
+export LD_LIBRARY_PATH=$HOME/anaconda3/envs/D-SD/lib
+wandb login 4baa24c4fc6c8eed782cacb721d34977149d4fcb
+python distill.py -t DSDI -n CIN_snellius_1e-9_100000_schedule_ALR
+
