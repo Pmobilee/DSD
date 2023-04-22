92a47f7725442d8fa9dcd4b4301eedff4178b5c9
Pmobilee <muldamion@gmail.com>
2023-04-22 12:58:01 +0200

    small

--- cin_256/self_distillation.py
+++ cin_256/self_distillation.py
@@ -367,7 +367,7 @@ def self_distillation_CELEB(student, sampler_student, original, sampler_original
                                                 
                                                 losses.append(loss.item())
                                             
-                                        if session != None and generation % 50 == 0:
+                                        if session != None and generation % 5 == 0:
                                                 
                                             x_T_teacher_decode = sampler_student.model.decode_first_stage(pred_x0_teacher)
                                             teacher_target = torch.clamp((x_T_teacher_decode+1.0)/2.0, min=0.0, max=1.0)
