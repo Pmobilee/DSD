4f1be3371bdfb3f71125f516f8aa9569d1208d13
Pmobilee <muldamion@gmail.com>
2023-09-03 18:32:37 +0200

    snel

--- DSD/SI_Batch.sh
+++ DSD/SI_Batch.sh
@@ -19,5 +19,5 @@ conda activate D-SD
 cd $Home/thesis/Diffusion_Thesis/cin_256
 export LD_LIBRARY_PATH=$HOME/anaconda3/envs/D-SD/lib
 wandb login 
-python distill.py -t DSDN -m cin -s 64 -lr 0.0000001 -u 4000 -pred True
+python distill.py -t DSDN -m cin -s 64 -lr 0.0000001 -u 10000 -pred True
 
