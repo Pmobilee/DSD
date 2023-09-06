d7f23a1eba3fa067b7e278ad0a56ed05c14956db
Pmobilee <muldamion@gmail.com>
2023-09-06 15:04:36 +0200

    snel

--- DSD/self_distillation.py
+++ DSD/self_distillation.py
@@ -136,8 +136,8 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                                                                 total_steps = total_steps)
                                             
                                             # Code below first decodes the latent image and then reconstructs it. This is not necessary, but can be used to check if the latent image is correct
-                                            decode_student = sampler_student.model.differentiable_decode_first_stage(pred_x0_student)
-                                            reconstruct_student = torch.clamp((decode_student+1.0)/2.0, min=0.0, max=1.0)
+                                            # decode_student = sampler_student.model.differentiable_decode_first_stage(pred_x0_student)
+                                            # reconstruct_student = torch.clamp((decode_student+1.0)/2.0, min=0.0, max=1.0)
                                             
                                          
 
@@ -157,8 +157,8 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                                                             steps_per_sampling = 1,
                                                                             total_steps = total_steps)     
 
-                                                decode_teacher = sampler_student.model.decode_first_stage(pred_x0_teacher)
-                                                reconstruct_teacher = torch.clamp((decode_teacher+1.0)/2.0, min=0.0, max=1.0)
+                                                # decode_teacher = sampler_student.model.decode_first_stage(pred_x0_teacher)
+                                                # reconstruct_teacher = torch.clamp((decode_teacher+1.0)/2.0, min=0.0, max=1.0)
                                         
 
                                      
@@ -181,8 +181,8 @@ def self_distillation_CIN(student, sampler_student, original, sampler_original, 
                                             noise = 1 - at
                                             log_snr = torch.log(signal / noise)
                                             weight = max(log_snr, 1)
-                                            # loss = weight * criterion(pred_x0_student, pred_x0_teacher.detach())     
-                                            loss = weight * criterion(reconstruct_student, reconstruct_teacher.detach())                    
+                                            loss = weight * criterion(pred_x0_student, pred_x0_teacher.detach())     
+                                            # loss = weight * criterion(reconstruct_student, reconstruct_teacher.detach())                    
                                             loss.backward()
                                             optimizer.step()
                                             scheduler.step()
