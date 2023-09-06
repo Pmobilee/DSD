f47a539611316ddcef636cb575f3184fd14bf6f7
Pmobilee <muldamion@gmail.com>
2023-09-06 18:23:13 +0200

    removed requires_grad

--- DSD/ldm/models/diffusion/ddim.py
+++ DSD/ldm/models/diffusion/ddim.py
@@ -487,10 +487,10 @@ class DDIMSampler(object):
         sqrt_one_minus_alphas = self.model.sqrt_one_minus_alphas_cumprod if use_original_steps else self.ddim_sqrt_one_minus_alphas
         sigmas = self.model.ddim_sigmas_for_original_num_steps if use_original_steps else self.ddim_sigmas
         # select parameters corresponding to the currently considered timestep
-        a_t = torch.full((b, 1, 1, 1), alphas[index], device=device, requires_grad=True)
-        a_prev = torch.full((b, 1, 1, 1), alphas_prev[index], device=device, requires_grad=True)
-        sigma_t = torch.full((b, 1, 1, 1), sigmas[index], device=device, requires_grad=True)
-        sqrt_one_minus_at = torch.full((b, 1, 1, 1), sqrt_one_minus_alphas[index],device=device, requires_grad=True)
+        a_t = torch.full((b, 1, 1, 1), alphas[index], device=device)
+        a_prev = torch.full((b, 1, 1, 1), alphas_prev[index], device=device)
+        sigma_t = torch.full((b, 1, 1, 1), sigmas[index], device=device)
+        sqrt_one_minus_at = torch.full((b, 1, 1, 1), sqrt_one_minus_alphas[index],device=device)
 
         # current prediction for x_0
         pred_x0 = (x - sqrt_one_minus_at * e_t) / a_t.sqrt()
@@ -500,7 +500,7 @@ class DDIMSampler(object):
         dir_xt = (1. - a_prev - sigma_t**2).sqrt() * e_t
         noise = sigma_t * noise_like(x.shape, device, repeat_noise) * temperature
         if noise_dropout > 0.:
-            noise = torch.nn.functional.dropout(noise, p=noise_dropout, requires_grad=True)
+            noise = torch.nn.functional.dropout(noise, p=noise_dropout)
         x_prev = a_prev.sqrt() * pred_x0 + dir_xt + noise
         v_ground_truth = a_t * e_t - sigma_t * x
         return x_prev, pred_x0, a_t, sqrt_one_minus_at, v_ground_truth
